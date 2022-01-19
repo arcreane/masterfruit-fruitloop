461ce68e4bd5ca3c9ab92ed7f55f9b406c3cd4c5
Flora JULIE <flora.juli88@gmail.com>
2022-01-19 10:37:27 +0100

    Skeleton of indices

--- app/src/main/java/com/example/fruitloop/StartGame.java
+++ app/src/main/java/com/example/fruitloop/StartGame.java
@@ -1,8 +1,13 @@
 package com.example.fruitloop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class StartGame extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class StartGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.indice_1:
+                Toast.makeText(this, "Indice 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.indice_2:
+                Toast.makeText(this, "Indice 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
