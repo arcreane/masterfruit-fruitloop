2b7a006515ae514ad4d53f7adb902d683102618e
angelrafael <generalpompeyo@hotmail.com>
2022-01-21 11:36:36 +0100

    Added several featurs to the game: -Creation of game fruit array for player to discover -Creation of player selection fruit array based on his selection -Added interactive icons -Added context menu for player to choose his fruits -Added try counter -Other improvements

--- app/src/main/java/com/example/fruitloop/Fruits.java
+++ app/src/main/java/com/example/fruitloop/Fruits.java
@@ -1,16 +1,18 @@
 package com.example.fruitloop;
 
 public enum Fruits {
-    BANANA("@drawable/banana"),
-    GRAPE("@drawable/grape"),
-    KIWI("@drawable/kiwi"),
-    LEMON("@drawable/lemon"),
-    ORANGE("@drawable/orange"),
-    PRUNE("@drawable/prune"),
-    RASPBERRY("@drawable/raspberry"),
-    STRAWBERRY("@drawable/strawberry");
+    BANANA(R.drawable.banana),
+    GRAPE(R.drawable.grape),
+    KIWI(R.drawable.kiwi),
+    LEMON(R.drawable.lemon),
+    ORANGE(R.drawable.orange),
+    PRUNE(R.drawable.prune),
+    RASPBERRY(R.drawable.raspberry),
+    STRAWBERRY(R.drawable.strawberry);
+    final int fruitId;
 
-    Fruits(String imgSrc) {
+    Fruits(int fruitId) {
+        this.fruitId = fruitId;
     }
 
 }
--- app/src/main/java/com/example/fruitloop/StartGame.java
+++ app/src/main/java/com/example/fruitloop/StartGame.java
@@ -1,27 +1,124 @@
 package com.example.fruitloop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Random;
 
 public class StartGame extends AppCompatActivity {
+    LinkedHashMap<Integer, Fruits> completeFruitList = new LinkedHashMap<>();
+    LinkedHashMap<Integer, Fruits> playerGuessListMenu = new LinkedHashMap<>();
+    Integer randomKey;
+    int playerTryCounter = 10;
+    int playerScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+        // Score elements
+        TextView txtScore = findViewById(R.id.txtVScore);
+        TextView txtTriesLeft = findViewById(R.id.txtVTriesLeft);
+        txtTriesLeft.setText(String.valueOf(playerTryCounter));
+
+        // We start by creating a list that contains all the fruits
+        completeFruitList.put(R.id.mi_strawberry, Fruits.STRAWBERRY);
+        completeFruitList.put(R.id.mi_orange, Fruits.ORANGE);
+        completeFruitList.put(R.id.mi_prune, Fruits.PRUNE);
+        completeFruitList.put(R.id.mi_kiwi, Fruits.KIWI);
+        completeFruitList.put(R.id.mi_lemon, Fruits.LEMON);
+        completeFruitList.put(R.id.mi_banana, Fruits.BANANA);
+        completeFruitList.put(R.id.mi_grape, Fruits.GRAPE);
+        completeFruitList.put(R.id.mi_raspberry, Fruits.RASPBERRY);
 
         // Here we create the fruit array that the player needs to discover
-        // Here we create a list that contains all the fruits
-        ArrayList<Fruits> fruitArray = new ArrayList<Fruits>();
+        LinkedHashMap<Integer, Fruits> randomFruitSelection = new LinkedHashMap<>();
+        Random rand = new Random();
+        while (randomFruitSelection.size() < 4) {
+            Object[] keys = completeFruitList.keySet().toArray();
+            randomKey = (Integer) keys[rand.nextInt(keys.length)];
+            if(!randomFruitSelection.containsKey(randomKey)){
+                randomFruitSelection.put(randomKey, completeFruitList.get(randomKey));
+            }
+        }
 
+        // Here we access to our imageViews
+        ImageView imgVFruit1 = findViewById(R.id.imgVFruit1);
+        ImageView imgVFruit2 = findViewById(R.id.imgVFruit2);
+        ImageView imgVFruit3 = findViewById(R.id.imgVFruit3);
+        ImageView imgVFruit4 = findViewById(R.id.imgVFruit4);
+        registerForContextMenu(imgVFruit1);
+        registerForContextMenu(imgVFruit2);
+        registerForContextMenu(imgVFruit3);
+        registerForContextMenu(imgVFruit4);
 
+        Button btnValidation = findViewById(R.id.btnValidation);
+        btnValidation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Here we check if the players answer is correct
+                if(randomFruitSelection.equals(playerGuessListMenu)) {
+                    Toast.makeText(getApplicationContext(), "You win!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Try Again", Toast.LENGTH_SHORT).show();
+                }
+                playerTryCounter -= 1;
+                txtTriesLeft.setText(String.valueOf(playerTryCounter));
+                if(playerTryCounter == 0) {
+                }
+            }
+        });
+    }
 
+    // Here we add our context menu to change the fruits in our row
+    ImageView selectedImageView;
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Choose a fruit");
+        getMenuInflater().inflate(R.menu.fruit_selection_menu, menu);
+        selectedImageView = (ImageView) v;
+    }
 
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        selectedImageView.setImageDrawable(null);
+        if(!item.getTitle().toString().contentEquals("Clear")) {
+            selectedImageView.setImageBitmap(((BitmapDrawable) item.getIcon()).getBitmap());
+            playerGuessListMenu.put(item.getItemId(),  getFruit(item.getTitle().toString().toUpperCase(Locale.ROOT)));
+            return true;
+        }
+        return true;
+    }
 
+    // Here we get a fruit object that matches the fruit name from the context menu
+    public Fruits getFruit(String fruitName) {
+        ArrayList<Fruits> fruitsArrayForMenu = new ArrayList<>();
+        fruitsArrayForMenu.add(Fruits.BANANA);
+        fruitsArrayForMenu.add(Fruits.GRAPE);
+        fruitsArrayForMenu.add(Fruits.KIWI);
+        fruitsArrayForMenu.add(Fruits.LEMON);
+        fruitsArrayForMenu.add(Fruits.PRUNE);
+        fruitsArrayForMenu.add(Fruits.RASPBERRY);
+        fruitsArrayForMenu.add(Fruits.STRAWBERRY);
+        fruitsArrayForMenu.add(Fruits.ORANGE);
 
-
+        for(int i = 0; i < fruitsArrayForMenu.size(); i++) {
+            if(fruitsArrayForMenu.get(i).toString().equals(fruitName)) {
+                return fruitsArrayForMenu.get(i);
+            }
+        }
+        return null;
     }
 }
