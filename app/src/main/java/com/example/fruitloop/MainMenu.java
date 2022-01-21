76d0b1352859917fe3f6de96de37530256040ede
Sofie <92948674+sofiefrance@users.noreply.github.com>
2022-01-21 16:10:45 +0100

    Beginning of xml score board

--- app/src/main/java/com/example/fruitloop/MainMenu.java
+++ app/src/main/java/com/example/fruitloop/MainMenu.java
@@ -1,6 +1,7 @@
 package com.example.fruitloop;
 
-import androidx.appcompat.app.AppCompatActivity;
+import
+        androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
